3819a840c4163bf20124fad658d55cd372c13271
siddharth369p <sidpathak2002@gmail.com>
2026-09-22 12:55:11 +0530

    making chatbot and adding llm model

--- llm_test.py
+++ llm_test.py
@@ -1,3 +1,16 @@
 import os 
-from .env import load_dotenv
-from openai import OpenAi 
+from dotenv import load_dotenv
+from openai import OpenAI 
+
+load_dotenv()
+HF_token=os.getenv("HF_TOKEN")
+
+client=OpenAI(base_url="https://router.huggingface.co/v1",api_key=HF_token)
+
+response=client.chat.completions.create(model="openai/gpt-oss-120b",
+                               messages=[{
+                                 "role":"user",
+                                 "content":"what is best diet plan for human being?"
+                               }])
+
+answer=response.choices[0].message.content
